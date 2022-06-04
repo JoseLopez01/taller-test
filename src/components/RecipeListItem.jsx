6d10713d30c14796e226be348017caa19ae82ea7
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2022-06-04 17:30:33 -0500

    Last style on list

--- src/components/RecipeListItem.jsx
+++ src/components/RecipeListItem.jsx
@@ -1,14 +1,27 @@
 import Avatar from '@mui/material/Avatar';
 import IconButton from '@mui/material/IconButton';
-
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
 import ListItemText from '@mui/material/ListItemText';
 import Save from '@mui/icons-material/Save';
+import { styled } from '@mui/material/styles';
+
+const Container = styled(ListItem)(() => ({
+  '&:hover': {
+    backgroundColor: '#f5f5f5',
+    cursor: 'pointer',
+  },
+  '&:first-of-type': {
+    borderRadius: '4px 4px 0 0',
+  },
+  '&:last-of-type': {
+    borderRadius: '0 0 4px 4px',
+  },
+}));
 
 function RecipeListItem({ name, image, source }) {
   return (
-    <ListItem
+    <Container
       disableGutters
       divider
       secondaryAction={
@@ -21,7 +34,7 @@ function RecipeListItem({ name, image, source }) {
         <Avatar alt={name} src={image} />
       </ListItemAvatar>
       <ListItemText primary={name} secondary={source} />
-    </ListItem>
+    </Container>
   );
 }
 
