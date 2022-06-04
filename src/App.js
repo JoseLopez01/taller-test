1475597073379d5c870222033f16ea443a3dbd9f
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2022-06-04 16:08:57 -0500

    starting

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Container from '@mui/material/Container';
+import { ThemeProvider, createTheme, styled } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+});
+
+const SearchContainer = styled('div')(() => ({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  marginTop: '20px',
+  width: '50%',
+}));
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Container>
+        <Typography variant="h4" align="center">
+          Recipes App
+        </Typography>
+        <SearchContainer>
+          <TextField
+            label="Search"
+            placeholder="Search a recipe"
+            variant="outlined"
+            color="secondary"
+            fullWidth
+          />
+        </SearchContainer>
+      </Container>
+    </ThemeProvider>
   );
 }
 
