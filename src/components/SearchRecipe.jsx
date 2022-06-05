41a0621132bf72f187f56ccdef6b19c9ad455bbe
Jose Lopez Ariza <lopezarizajosealejandro@gmail.com>
2022-06-04 19:02:32 -0500

    search

--- src/components/SearchRecipe.jsx
+++ src/components/SearchRecipe.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useMemo, useState } from 'react';
 import debounce from 'lodash.debounce';
 
+import Autocomplete from '@mui/material/Autocomplete';
 import TextField from '@mui/material/TextField';
+import CircularProgress from '@mui/material/CircularProgress';
 import { styled } from '@mui/material/styles';
 
 import searchRecipes from '../helpers/search-recipes';
@@ -13,8 +15,12 @@ const SearchContainer = styled('div')(() => ({
   width: '100%',
 }));
 
-function SearchRecipe({ setRecipes }) {
+function SearchRecipe() {
   const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+  const [open, setOpen] = useState(false);
+
+  const loading = open && search.length > 0 && searchResults.length === 0;
 
   const handleChange = (event) => {
     setSearch(event.target.value);
@@ -31,21 +37,43 @@ function SearchRecipe({ setRecipes }) {
   useEffect(() => {
     if (search) {
       searchRecipes(search).then((recipes) => {
-        setRecipes(recipes);
+        setSearchResults(recipes);
       });
     }
-  }, [search, setRecipes]);
+  }, [search]);
 
   return (
     <SearchContainer>
-      <TextField
-        label="Search"
-        placeholder="Search a recipe"
-        variant="outlined"
-        color="secondary"
-        fullWidth
-        defaultValue=""
-        onChange={debouncedSearch}
+      <Autocomplete
+        sx={{ width: '100%' }}
+        open={open}
+        onOpen={() => setOpen(true)}
+        onClose={() => setOpen(false)}
+        isOptionEqualToValue={(option, value) => option.id === value}
+        getOptionLabel={(option) => option.name}
+        options={searchResults}
+        loading={loading}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Search"
+            placeholder="Search a recipe"
+            variant="outlined"
+            color="secondary"
+            onChange={debouncedSearch}
+            InputProps={{
+              ...params.InputProps,
+              endAdornment: (
+                <>
+                  {loading ? (
+                    <CircularProgress color="inherit" size={20} />
+                  ) : null}
+                  {params.InputProps.endAdornment}
+                </>
+              ),
+            }}
+          />
+        )}
       />
     </SearchContainer>
   );
